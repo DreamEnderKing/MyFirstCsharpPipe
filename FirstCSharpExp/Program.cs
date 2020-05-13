89feb37bbcc5204420844542528cd742807be86b
www20040718 <https://www20040718@dev.azure.com/www20040718/DreamSystem/_git/DreamSystem>
2020-05-13 17:13:21 +0800

    123

---
+++ FirstCSharpExp/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2020/5/13
+ * 时间: 16:58
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace FirstCSharpExp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, the World!");
+			Console.WriteLine("The github action is running!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
