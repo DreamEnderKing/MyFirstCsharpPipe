67214faa2ece9c5e8c10163900327e2c404d1b52
www20040718 <https://www20040718@dev.azure.com/www20040718/DreamSystem/_git/DreamSystem>
2020-05-15 09:17:39 +0800

    123

--- FCsharp/Program.cs
+++ FCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FCsarp
 {
@@ -7,6 +8,10 @@ namespace FCsarp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            if(!File.Exists("log.txt")) File.Create("log.txt");
+            FileStream stream = new FileStream("log.txt", FileMode.Append);
+            Console.SetOut(new StreamWriter(stream));
+            Console.WriteLine("hello world!");
         }
     }
 }
