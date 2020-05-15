c5863406fe476b2dde4da169b35de5d73cab2094
www20040718 <https://www20040718@dev.azure.com/www20040718/DreamSystem/_git/DreamSystem>
2020-05-15 20:31:34 +0800

    123

--- FCsharp/Program.cs
+++ FCsharp/Program.cs
@@ -15,9 +15,17 @@ namespace FCsarp
                 stream = new FileStream("log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
                 StreamWriter _writer = new StreamWriter(stream);
                 _writer.WriteLine("hello,world!");
+                _writer.Flush();
+                _writer.Dispose();
                 
-            } finally {
+            } 
+            catch(Exception e){
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("error!\nMessage:{0}", e.Message);
+            }
+            finally {
                 stream.Dispose();
+                Console.ForegroundColor = ConsoleColor.White;
             }
         }
     }
