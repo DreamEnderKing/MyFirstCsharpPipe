b9106e1eba2479bb60704a3e3cc7af833d5dc059
www20040718 <https://www20040718@dev.azure.com/www20040718/DreamSystem/_git/DreamSystem>
2020-05-15 15:35:24 +0800

    123

--- FCsharp/Program.cs
+++ FCsharp/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace FCsarp
 {
     class Program
     {
+        static FileStream stream;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            if(!File.Exists("log.txt")) File.Create("log.txt");
-            FileStream stream = new FileStream("log.txt", FileMode.Append, FileAccess.ReadWrite, FileShare.ReadWrite);
-            Console.SetOut(new StreamWriter(stream));
-            Console.WriteLine("hello world!");
+            try {
+                if(!File.Exists("log.txt")) File.Create("log.txt");
+                stream = new FileStream("log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+                StreamWriter _writer = new StreamWriter(stream);
+                _writer.WriteLine("hello,world!");
+                
+            } finally {
+                stream.Dispose();
+            }
         }
     }
 }
