5a99b4899841ab345c816bc7ef4adba4e5790745
DreamEnderKing <58102615+DreamEnderKing@users.noreply.github.com>
2020-08-14 18:23:28 +0800

    Update Program.cs

--- FCsharp/Program.cs
+++ FCsharp/Program.cs
@@ -4,67 +4,15 @@ using System.Text;
 
 namespace FCsharp
 {
-    public enum loggerType
-    {
-        ErrorMsg,
-        WarningMsg,
-        InfoMsg
-    }
-    class logger
-    {
-        public logger(string filepath)
-        {
-            if(!File.Exists("log.txt")) File.Create("log.txt");
-            _stream = new FileStream("log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
-            _writer = new StreamWriter(_stream);
-            
-        }
-        private Stream _stream;
-        private StreamWriter _writer;
-        public void Write(object o,loggerType lt)
-        {
-            switch (lt) {
-                case loggerType.ErrorMsg:
-                    _writer.WriteLine("[Error:{0}]", DateTime.Now.ToString());
-                    break;
-                case loggerType.WarningMsg:
-                    _writer.WriteLine("[Warning:{0}]", DateTime.Now.ToString());
-                    break;
-                case loggerType.InfoMsg:
-                    _writer.WriteLine("[Information:{0}]", DateTime.Now.ToString());
-                    break;
-                default:
-                    throw new Exception("Invalid value for loggerType");
-            }
-            _writer.WriteLine(o);
-            _writer.Flush();
-        }
-        public void Write(object o)
-        {
-            Write(o, loggerType.InfoMsg);
-        }
-        public void Dispose()
-        {
-            _writer.Dispose();
-            _stream.Dispose();
-        }
-    }
-    
     class Program
     {
-        public static logger log = new logger("log.txt");
         static void Main(string[] args)
         {
-            Console. WriteLine("Hello world");
             try {
-                log.Write("info msg!");
-                log.Write("warning msg!",loggerType.WarningMsg);
-                //zero divided
-                int c = 24 - 4 * 6;
-                int i = 12 / c;
+                Console. WriteLine(TimeSpan.Parse("00:00:01"));
             } 
             catch(Exception e){
-                log.Write(e.Message, loggerType.ErrorMsg);
+                Console. WriteLine(e.Message);
             }
             finally {
                 Console.ForegroundColor = ConsoleColor.White;
@@ -73,7 +21,7 @@ namespace FCsharp
             
         ~Program()
         {
-            log.Dispose();
+            
         }
     }
 }
